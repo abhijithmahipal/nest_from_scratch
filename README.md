59a2216bbefdb91daeff315aad5725f767b5aa97
abhijithmahipal <abhijithmahipal@gmail.com>
2022-07-24 14:46:07 +0530

    added minimum api

---
+++ README.md
@@ -0,0 +1,1 @@
+Minimal nestjs api
---
+++ src/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from "@nestjs/common";
+
+@Controller("app")
+export class AppController {
+  @Get("hello")
+  getRootRoute() {
+    return "Hello";
+  }
+
+  @Get("bye")
+  getBye() {
+    return "Bye";
+  }
+}
